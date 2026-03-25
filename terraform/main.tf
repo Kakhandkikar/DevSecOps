df5f260ff0f93ad03cfc1819962868ebc5b2e83b
Kakhandkikar <143871690+Kakhandkikar@users.noreply.github.com>
2026-03-25 12:07:21 +0530

    Remove bucket_name output from main.tf
    Remove output for bucket name in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,4 @@ resource "aws_s3_bucket" "test_bucket" {
   bucket = "vault-demo-bucket-${random_id.bucket_suffix.hex}"
 }
 
-output "bucket_name"{
-    value = aws_s3_bucket.vault_test_bucket.id
-}
 
